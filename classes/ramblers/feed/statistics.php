ca67ade9745261b449291446728387883cf99bfd
Chris Vaughan <chris@cevsystems.co.uk>
2015-04-04 08:48:09 +0100

    update to splash and statistics

--- classes/ramblers/feed/statistics.php
+++ classes/ramblers/feed/statistics.php
@@ -11,12 +11,14 @@ class RamblersFeedStatistics {
     private $area;
     private $walks;
     private $groups;
+    private $groupsblankcontact;
 
     function __construct($db, $area, $walks) {
         $this->db = $db;
         $this->area = $area;
         $this->walks = $walks;
         $this->groups = array();
+        $this->groupsblankcontact = array();
     }
 
     function Display() {
@@ -28,8 +30,23 @@ class RamblersFeedStatistics {
                     $this->groups[$groupcode] = new RamblersFeedStatisticsgroup($groupcode, $groupname);
                 }
                 $this->groups[$groupcode]->addWalk($walk);
+                if ($walk->walkContact != null) {
+                                $contactName = $walk->walkContact->contact->displayName;
+                           } else {
+                               
+                           }
+                
+                if ($contactName == "") {
+                    if (!isset($this->groupsblankcontact[$groupcode])) {
+                        $this->groupsblankcontact[$groupcode] = "";
+                    }
+                    $this->groupsblankcontact[$groupcode] .= $walk->id . ", ";
+                }
             }
             $json = json_encode($this->groups, JSON_PRETTY_PRINT);
+            Logfile::write($json);
+            logfile::write("Walks with blank contact display name");
+            $json = json_encode($this->groupsblankcontact, JSON_PRETTY_PRINT);
             Logfile::write($json);
             $this->db->updateAreaStatistics($this->area, $json);
             echo $json;
