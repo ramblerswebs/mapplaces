144d6f6196e7834824169e956100df7653129b0b
Chris Vaughan <webmaster@ramblers-webs.org.uk>
2016-08-20 16:07:03 +0100

    set correct file name in control

--- classes/places/control.php
+++ classes/places/control.php
@@ -8,13 +8,13 @@
 class PlacesControl {
 
     private $mLastAreaProcessed = "fred";
-    private $filename = "notset";
+   // private $filename = "notset";
     const controlfilename="control/control.json";
 
     public function __construct() {
-        $this->filename = getcwd() . self::controlfilename;
-        if (file_exists($this->filename)) {
-            $storage = file_get_contents($this->filename);
+       // $this->filename = getcwd() . self::controlfilename;
+        if (file_exists(self::controlfilename)) {
+            $storage = file_get_contents(self::controlfilename);
             if ($storage != "") {
                 $json = json_decode($storage);
                 //echo "area : " . $json->lastAreaProcessed;
@@ -30,21 +30,13 @@ class PlacesControl {
     public function updateLastAreaProcessed($area) {
         $this->mLastAreaProcessed = $area->getCode();
         $json = '{"lastAreaProcessed":"' . $area->getCode(). '"}';
-        file_put_contents($this->filename, $json);
+        file_put_contents(self::controlfilename, $json);
     }
 
     public static function getDateAreaLastUpdated($areacode) {
         // find date area log file was last updated
         $logfile = "logfiles/logfile_area_" . $areacode . ".log";
         return PlacesFunctions::getDateFileLastUpdated($logfile);
-        if (file_exists($logfile)) {
-            $timestamp= filemtime($logfile);
-            $lastrun=new DateTime();
-            $lastrun=$lastrun->setTimestamp ($timestamp);
-            return $lastrun;
-        } else {
-            return new DateTime('2000-01-01');
-        }
     }
 
 }
