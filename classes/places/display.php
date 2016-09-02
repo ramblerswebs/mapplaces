80fad096031381e754f28da84b2197b68c356867
Chris Vaughan <webmaster@ramblers-webs.org.uk>
2016-09-02 08:38:58 +0100

    add cache for the retrieval of locations

--- classes/places/display.php
+++ classes/places/display.php
@@ -16,24 +16,51 @@ class PlacesDisplay {
     private $db;
     private $stars;  // array defining which starred walks should be displayed
     private $age;
+    private $agedate;
+    private $compare;
 
     public function __construct($database, $stars, $age) {
         $this->db = $database;
         $this->stars = $stars;
         $this->age = $age;
+        $this->compare = "newer";
+        if ($this->age == "5older") {
+            $this->compare = "older";
+        }
+        $this->agedate = PlacesFunctions::getAgeDate($this->age);
+    }
+
+    private function getCacheFileName() {
+        $name = "cache/points";
+        for ($index = 0; $index < count($this->stars); $index++) {
+            if ($this->stars[$index]) {
+                $name.="_star" . $index;
+            }
+        }
+        $name.=$this->age;
+        $name.=$this->compare;
+        return $name . ".cache";
+    }
+
+    private function getPlaces() {
+        $cachefile = $this->getCacheFileName();
+        $lastupdated = PlacesFunctions::getDateFileLastUpdated($cachefile);
+        $yesterday = new DateTime("yesterday");
+        if ($lastupdated > $yesterday) {
+            $points = file_get_contents($cachefile);
+        } else {
+            $points = $this->db->getPlaces($this->stars, $this->agedate, $this->compare);
+            file_put_contents($cachefile, $points);
+        }
+        return $points;
     }
 
     public function display($menu) {
         $template = new Template("dist/mapTemplate.html");
         $template->replaceString("// [[Insert menu]]", $menu->getMenu(Null));
         $template->replaceStringWithFile("// [[Insert Analytics]]", "analyticstracking.php");
-        $compare = "newer";
-        if ($this->age == "5older") {
-            $compare = "older";
-        }
-
-        $agedate = PlacesFunctions::getAgeDate($this->age);
-        $points = $this->db->getPlaces($this->stars, $agedate, $compare);
+        $points = $this->getPlaces();
+        //  $points = $this->db->getPlaces($this->stars, $this->agedate, $this->compare);
         $template->replaceString("// [[Add markers here]]", $points);
         $template->displayTemplate();
     }
