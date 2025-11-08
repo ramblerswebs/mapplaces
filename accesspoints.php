1c515207b8ff7e60c58fc6fa8d7db33c4b6d2946
Chris Vaughan <chris@cevsystems.co.uk>
2025-11-08 08:07:48 +0000

    Version 09

---
+++ accesspoints.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * List of access points for meeting/start locations
+ * 
+ * task.php
+ * get.php  easting=564400&northing=219900&dist=10&maxpoints=10
+ * getall.php
+ * report.php?gr=SK123456&type=like
+ * report.php?gr=SK123456&type=dislike
+*/
